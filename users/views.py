40f316720996812d5b49b206cf172f9e29db828e
Chaitra Adiga <itsca03@gmail.com>
2024-11-19 16:28:12 +0530

    Logout setup

--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username=form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f' Your Account created , Please Login !')
+            return redirect('login')
     else:
         form=UserRegisterForm() # making an instance of default form
     return render(request,'users/register.html',{'form':form}) #passing the instance as context
